0271a8ef9961b1505463e280a9669ede6c004157
Pavel Ponec <pponec@users.noreply.github.com>
2008-12-06 08:18:02 +0000

    Module support

---
+++ src/net/ponec/module/SampleModule.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.ponec.module;
+
+import net.ponec.jworksheet.core.ApplContextInterface;
+
+/**
+ * An sample moduel
+ * @author pavel
+ */
+public class SampleModule implements Runnable {
+    
+    private ApplContextInterface context;
+    
+    public SampleModule(ApplContextInterface context) {
+        this.context = context;
+    }
+
+    public void run() {
+        if (context.isStarting()) {
+            System.out.println("STATING jWorkSheet");
+        } else {
+            System.out.println("FINISHING jWorkSheet");
+        }
+    }
+}
