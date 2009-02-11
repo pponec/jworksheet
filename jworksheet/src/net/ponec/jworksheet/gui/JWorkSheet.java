4276cce5bc7ad1c48b04fe68ce8001e38f5a6e50
Pavel Ponec <pponec@users.noreply.github.com>
2009-02-11 20:09:52 +0000

    UJO sub release

--- jworksheet/src/net/ponec/jworksheet/gui/JWorkSheet.java
+++ jworksheet/src/net/ponec/jworksheet/gui/JWorkSheet.java
@@ -75,8 +75,8 @@ import org.ujoframework.swing.UjoPropertyRow;
 @SuppressWarnings("unchecked")
 public final class JWorkSheet extends TopFrame {
     
-    public static final String APPL_VERSION  = "0.82.5" ;
-    public static final String APPL_RELEASED = "2009/02/09";
+    public static final String APPL_VERSION  = "0.82.6" ;
+    public static final String APPL_RELEASED = "2009/02/11";
     
     public static final String APPL_NAME     = "jWorkSheet";
     public static final String APPL_HOMEPAGE = "http://jworksheet.ponec.net/";
