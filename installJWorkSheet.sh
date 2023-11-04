c7b2e0c2d1d791574ee59359705efe2774a7db2a
pponec <pponec@gmail.com>
2023-11-04 15:04:05 +0100

    Default installation directory is: $HOME/opt/jworksheet-master

--- installJWorkSheet.sh
+++ installJWorkSheet.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-# Installations to the new Ubuntu:
+# Install the jWorkSheet to the Ubuntu:
 
 set -e
-
+targetDir=$HOME/opt
+runnerScript=jws.sh
 
 # jWorkSheet
 file=jworkhseet.zip
-mkdir -p ~/bin && cd ~/bin
+mkdir -p $targetDir && cd $targetDir
 wget -O $file https://github.com/pponec/jworksheet/archive/refs/heads/master.zip
 unzip -o $file
 rm $file
 cd jworksheet-master
 ./mvnw install -DskipTests
+
+# Create a runner script:
+echo "#!/bin/sh\njava -jar $(ls $PWD/target/*ies.jar)" > ../$runnerScript
+chmod 755 ../$runnerScript
 
